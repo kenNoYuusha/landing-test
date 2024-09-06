f26fa5481c40a2201a0e03e61b25109788bdfe10
Jluis <jorge_luis1709@hotmail.com>
2024-09-05 22:30:48 -0600

    astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://kennoyuusha.github.io',
+  base: '/landing-test'
+
 });
